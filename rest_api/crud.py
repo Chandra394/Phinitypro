10cd5828a36886a6c522fa9eddd678d0abbe137f
Chandra394 <78444052+Chandra394@users.noreply.github.com>
2021-02-25 01:11:30 -0800

    Add files via upload

---
+++ rest_api/crud.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import Session
+
+from models import Base , User 
+import schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.hashed_password + ""
+    db_user = User(firstname=user.firstname, lastname=user.lastname, username=user.username, email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+# def get_items(db: Session, skip: int = 0, limit: int = 100):
+#     return db.query(Item).offset(skip).limit(limit).all()
+
+
+# def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+#     db_item = Item(**item.dict(), owner_id=user_id)
+#     db.add(db_item)
+#     db.commit()
+#     db.refresh(db_item)
+#     return db_item
---
+++ rest_api/schemas.py
@@ -0,0 +1,45 @@
+
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+
+    firstname: str
+    lastname: str
+    username: str
+    email:str
+    hashed_password: str
+
+class UserCreate(UserBase):
+    firstname: str
+    lastname: str
+    username: str
+    email: str
+    hashed_password: str
+     
+
+class User(UserBase):
+    id: int
+    # is_active: bool
+    # items: List[Item] = []
+
+    class Config:
+        orm_mode = True
+
+# from pydantic.types import StrIntFloat
+
+
+# class ItemBase(BaseModel):
+#     title: str
+#     description: Optional[str] = None
+
+
+# class ItemCreate(ItemBase):
+#     pass
+
+
+# class Item(ItemBase):
+#     id: int
+#     owner_id: int
+
+#     class Config:
+#         orm_mode = True        
